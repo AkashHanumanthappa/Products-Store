073f8d9534d1e9b6f8c04a8e8b12a65dc87bf470
AkashHanumanthappa <akashhanumanthappa.official@gmail.com>
2025-05-16 01:01:59 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 
@@ -17,7 +16,6 @@ app.use(cors({
 }));
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));       // for JSON payloads
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -31,12 +29,6 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);      // Mount user routes (register, login, admin, etc.)
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
 	console.log("Server started at http://localhost:" + PORT);
