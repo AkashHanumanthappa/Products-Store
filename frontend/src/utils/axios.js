3b3d419971b70d62d2cf06a4c82efd9cf2ceccfe
AkashHanumanthappa <akashhanumanthappa.official@gmail.com>
2025-05-15 23:05:55 +0530

    last

--- frontend/src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: '/api/user', // Gets forwarded to http://localhost:5000/api/user via Vite proxy
-  withCredentials: true,
-});
-
-export default instance;
