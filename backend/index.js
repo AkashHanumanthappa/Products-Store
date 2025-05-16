0faae00137ff4f85cbb18858de9de67c3c05aec0
AkashHanumanthappa <akashhanumanthappa.official@gmail.com>
2025-05-16 06:27:38 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -31,12 +31,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);      // Mount user routes (register, login, admin, etc.)
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+
 
 
 app.listen(PORT, () => {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://products-store-backend.onrender.com",
+      "/api": "http://localhost:5000",
     },
   },
 });
